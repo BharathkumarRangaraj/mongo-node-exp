7a7e16ae4d263058f373a6a08f602d5addfd3fdf
unknown <rbharathkumar12@gmail.com>
2024-04-12 18:38:59 +0530

    use body-parser as mddlware,create doc,post,form,,status code

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+//to pass nput as prop value
 const bodyparser = require("body-parser");
 const exhbs = require("express-handlebars");
 const dbase = require("./db.js");
@@ -10,9 +11,21 @@ app.engine(
 );
 app.set("view engine", "hbs");
 app.set("views", "views");
+//to make express app as mddleware
+app.use(bodyparser.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
-  let message = "test";
+  let message = "";
+
+  switch (req.query.status) {
+    case "1":
+      message = "nserted succesfully";
+
+      break;
+
+    default:
+      break;
+  }
   const database = await dbase.getDatabase();
   const collection = database.collection("books");
   const cursor = collection.find({});
@@ -20,6 +33,14 @@ app.get("/", async (req, res) => {
   res.render("main", { message, employees });
 });
 
+app.post("/store_book", async (req, res) => {
+  const database = await dbase.getDatabase();
+  const collection = database.collection("books");
+  //to get form datas
+  let book = { title: req.body.title, author: req.body.author };
+  await collection.insertOne(book);
+  return res.redirect("/?status=1");
+});
 app.listen(8000, () => {
   console.log("running port 8000");
 });
